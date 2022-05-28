ca8b893e4ad9bedc99b546b80703d3226c9c0e51
nullforce <glenngit@nullforce.com>
2022-05-28 12:28:18 -0500

    Add timer function scaffold

---
+++ src/DerpiDownload.Functions/SiteFunctions.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace DerpiDownload.Functions;
+
+public class SiteFunctions
+{
+    [FunctionName("Timer_QueueSiteTags")]
+    public void Run([TimerTrigger("0 */30 * * * *")] TimerInfo myTimer, ILogger log)
+    {
+        log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+    }
+}
